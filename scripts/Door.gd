71dcc65d21ec289507a0db033d1cbfc78a52c592
Matthew Poon <matthewdpoon@gmail.com>
2025-10-13 23:18:23 -0400

    Door to House

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -1,8 +1,26 @@
-#Door.gd
+##Door.gd
 extends Area2D
+#
+#signal door_knocked
+#
+#func _on_Door_body_entered(body):
+	#if body.name == "Player":
+		#emit_signal("door_knocked")
+		#
+#Track whether the player is close enough to interact
+var player_nearby: bool = false
 
-signal door_knocked
-
-func _on_Door_body_entered(body):
+func _on_body_entered(body):
 	if body.name == "Player":
-		emit_signal("door_knocked")
+		player_nearby = true
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_nearby = false
+
+func _process(delta):
+	# Check if the player is nearby AND presses the interact key ("E")
+	if player_nearby and Input.is_action_just_pressed("Interact"):
+		get_tree().change_scene_to_file("res://scenes/House1.tscn")
+		print("Poop")
+ 
