c37e8c84e3be6140ddbdfd2e0fc7bae477416736
dionariestanto <ariestantodion@gmail.com>
2025-10-14 00:21:35 -0400

    TransitionLayersProgress Cont.

--- TransitionLayer.gd
+++ TransitionLayer.gd
@@ -5,6 +5,8 @@ extends CanvasLayer
 func _ready():
 	# Fade in automatically when scene starts
 	fade_in()
+	await get_tree().create_timer(2.0).timeout
+	fade_out_and_change_scene("res://scenes/House_1.tscn")
 
 func fade_in():
 	fade_rect.modulate.a = 1.0
@@ -13,8 +15,8 @@ func fade_in():
 
 func fade_out_and_change_scene(new_scene_path: String):
 	var tween = get_tree().create_tween()
-	tween.tween_property(fade_rect, "modulate:a", 1.0, 1.0)  # Fade to black
-	tween.tween_callback(Callable(self, "_on_fade_complete"), new_scene_path)
+	tween.tween_property(fade_rect, "modulate:a", 1.0, 1.0)
+	tween.tween_callback(func(): _on_fade_complete(new_scene_path))
 
 func _on_fade_complete(new_scene_path: String):
 	get_tree().change_scene_to_file(new_scene_path)
